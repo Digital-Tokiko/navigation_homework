351aa0c0311fac0581e91e89250089d44e9569c2
Digital_Tokiko <huijunzan@nuaa.edu.cn>
2026-06-15 22:22:23 +0800

    Update:NavVelRemap和GoalSender的协调还需要做一下。但基础功能完成，以及Attack正在搭框架。

---
+++ src/robot_behavior_tree/include/Attack.h
@@ -0,0 +1,16 @@
+//
+// Created by mizu on 2026/6/15.
+//
+
+#ifndef NAVIGATION_HOMEWORK_ATTACK_H
+#define NAVIGATION_HOMEWORK_ATTACK_H
+
+
+
+class Attack {
+
+};
+
+
+
+#endif //NAVIGATION_HOMEWORK_ATTACK_H
---
+++ src/robot_behavior_tree/src/Attack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mizu on 2026/6/15.
+//
+
+#include "Attack.h"
